c0e2e978c5e7eb6fa9bca03f4dd476ed559bcb5c
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-06 19:08:12 +0300

    Edit game functionality to use factory functions

--- app.js
+++ app.js
@@ -1,161 +1,139 @@
-const cellDivs = document.querySelectorAll('.game-cell');
-let gameStart = true;
-let currentPlayer = true;
+const playerFactory = (name, mark) => {
+    const playTurn = (board, cell) => {
+      const idx = board.cells.findIndex(position => position === cell);
+      if (board.positions[idx] === '') {
+        board.render();
+        return idx;
+      }
+      return null;
+    };
+  
+    return { name, mark, playTurn };
+  };
 
-// functions
 
-const handleWin = (player) => {
-    const statusDiv = document.querySelector('.status');
-    gameStart = false;
-    if (player === 'x') {
-        statusDiv.innerHTML = `${player1} has won`;
-    } else {
-        statusDiv.innerHTML = `<span>${player2} has won</span>`;
-    }
-}
-
-const checkGameStatus = () => {
-    const topLeft = cellDivs[0].classList[1];
-    const topMiddle = cellDivs[1].classList[1];
-    const topRight = cellDivs[2].classList[1];
-    const middleLeft = cellDivs[3].classList[1];
-    const middleMiddle = cellDivs[4].classList[1];
-    const middleRight = cellDivs[5].classList[1];
-    const bottomLeft = cellDivs[6].classList[1];
-    const bottomMiddle = cellDivs[7].classList[1];
-    const bottomRight = cellDivs[8].classList[1];   
-    const statusDiv = document.querySelector('.status');
-    
-
-    // Check winner
-
-    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-      handleWin(topLeft); 
-      cellDivs[0].classList.add('won');
-      cellDivs[1].classList.add('won');
-      cellDivs[2].classList.add('won'); 
-    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-      handleWin(middleLeft);
-      cellDivs[3].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[5].classList.add('won'); 
-    }
-    else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-        handleWin(bottomLeft);
-        cellDivs[6].classList.add('won');
-        cellDivs[7].classList.add('won');
-        cellDivs[8].classList.add('won'); 
-    }else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-      cellDivs[3].classList.add('won');
-      cellDivs[6].classList.add('won'); 
-    }else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[8].classList.add('won'); 
-    }else if (topMiddle &&  topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-        handleWin(topMiddle);
-        cellDivs[1].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[7].classList.add('won'); 
-    }else if (topRight && topRight === middleRight && topRight === bottomRight) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-      cellDivs[5].classList.add('won');
-      cellDivs[8].classList.add('won'); 
-    }else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[6].classList.add('won'); 
-    }else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-        handleWin(bottomLeft);
-        cellDivs[6].classList.add('won');
-      cellDivs[7].classList.add('won');
-      cellDivs[8].classList.add('won'); 
-    } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle 
-               && middleRight && bottomLeft && bottomMiddle && bottomRight){
-              gameStart = false;
-              statusDiv.innerHTML = 'It is a Draw';
-    } else {
-        currentPlayer = !currentPlayer;
-        if (currentPlayer){
-            statusDiv.innerHTML = `${player1} is next`;
-        } else {
-            statusDiv.innerHTML = `<span>${player2} is next</span>`;
+  const boardModule = (() => {
+    let positions = ['', '', '', '', '', '', '', '', ''];
+    const gameBoard = document.querySelector('#board');
+    const cells = Array.from(document.querySelectorAll('.cell'));
+    let winner = null;
+  
+    const render = () => {
+      positions.forEach((mark, idx) => {
+        cells[idx].textContent = positions[idx];
+      });
+    };
+  
+    const reset = () => {
+      positions = ['', '', '', '', '', '', '', '', ''];
+    };
+  
+    const checkWinner = () => {
+      const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+  
+      winningCombinations.forEach((winningCombo) => {
+        if (positions[winningCombo[0]]
+          && positions[winningCombo[0]] === positions[winningCombo[1]]
+          && positions[winningCombo[0]] === positions[winningCombo[2]]) {
+          winner = 'current';
         }
-    }
-};
+      });
+      return winner || (positions.includes('') ? null : 'It\'s a draw');
+    };
+  
+    return {
+      render, gameBoard, cells, positions, checkWinner, reset,
+    };
+  })();
 
 
-// Clearing Items
+  const gameStart = (() => {
+    const playerOneName = document.querySelector('#player1');
+    const playerTwoName = document.querySelector('#player2');
+    const form = document.querySelector('.player-info');
+    const resetBtn = document.querySelector('#reset');
+    let currentPlayer;
+    let playerOne;
+    let playerTwo;
+  
+    const turnTaken = () => {
+      currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    };
+  
+    const gameRound = () => {
+      const board = boardModule;
+      const gameStatus = document.querySelector('.game-status');
+      if (currentPlayer.name !== '') {
+        gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+      } else {
+        gameStatus.textContent = 'Board: ';
+      }
+  
+      board.gameBoard.addEventListener('click', (event) => {
+        event.preventDefault();
+        const play = currentPlayer.playTurn(board, event.target);
+        if (play !== null) {
+          board.positions[play] = `${currentPlayer.mark}`;
+          board.render();
+          const winStatus = board.checkWinner();
+          if (winStatus === 'It\'s a draw') {
+            gameStatus.textContent = 'It\'s a draw!';
+          } else if (winStatus === null) {
+            turnTaken();
+            gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+          } else {
+            gameStatus.textContent = `Winner is ${currentPlayer.name}`;
+            board.reset();
+            board.render();
+          }
+        }
+      });
+    };
+  
+    const init = () => {
+      if (playerOneName.value !== '' && playerTwoName.value !== '') {
+        playerOne = playerFactory(playerOneName.value, 'X');
+        playerTwo = playerFactory(playerTwoName.value, 'O');
+        currentPlayer = playerOne;
+        gameRound();
+      }
+    };
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      if (playerOneName.value !== '' && playerTwoName.value !== '') {
+        init();
+        form.classList.add('hidden');
+        document.querySelector('.place').classList.remove('hidden');
+      } else {
+        window.location.reload();
+      }
+    });
+  
+    resetBtn.addEventListener('click', () => {
+      document.querySelector('.game-status').textContent = 'Board: ';
+      document.querySelector('#player1').value = '';
+      document.querySelector('#player2').value = '';
+      window.location.reload();
+    });
+    return {
+      init
+    };
+  })();
 
-const clearItems = () => {
-    const statusDiv = document.querySelector('.status');
-    player1 = document.getElementById('first-player');
-    player2 = document.getElementById('second-player');
-    player1.value = '';
-    player2.value = '';
-    statusDiv.innerHTML = '';
-}
 
-
-// Event handlers
-
-const handleReset = () => {
-    currentPlayer = true;
-    for (const cellDiv of cellDivs){
-        cellDiv.classList.remove('x');
-        cellDiv.classList.remove('o');
-        cellDiv.classList.remove('won');
-    }
-
-    handleStartButton();
-    gameStart = true;
-    clearItems();
-};
-
-const handleCellClick = (e) => {
-    const clicked = e.target.classList;
-
-    if ( !gameStart || clicked[1] === 'x' || clicked[1] === 'o'){
-        return;
-    }
-
-    if (currentPlayer) {
-        clicked.add('x');
-        checkGameStatus();
-    } else {
-        clicked.add('o');
-        checkGameStatus();
-    }
-};
-
-
-const handleStartButton = () => {
-    const gamePlayers = document.getElementById('players');
-    const newGame = document.getElementById('game');
-    player1 = document.getElementById('first-player').value;
-    player2 = document.getElementById('second-player').value;
-    if (player1 === '' && player2 === ''){
-        alert('Please enter names for player one and player two');
-        return;
-    }
-    newGame.classList.toggle('visible');
-    gamePlayers.classList.toggle('invisible');
- }
-
-
-// Event listeners
+  gameStart.init();
 
 
 
-for (const cellDiv of cellDivs){
-    cellDiv.addEventListener('click', handleCellClick);
-}
 
-document.getElementById('start').addEventListener('click', handleStartButton);
-document.querySelector('.reset').addEventListener('click', handleReset);
 
